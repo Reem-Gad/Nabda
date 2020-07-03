6f862cc0b775bab8bd536c21f217fd52ee985fc5
reemgad <reem34378@gmail.com>
2020-07-03 23:29:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/nabda/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.nabda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button butt;
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        butt = findViewById(R.id.but2);
+        button1=findViewById(R.id.but1);
+        butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go = new Intent(MainActivity.this,singUp.class);
+                startActivity(go);
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(go);
+            }
+        });
+
+
+    }
+}
